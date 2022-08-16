12c11028ce7035553f518a4e45e1194fb5c8c20c
MaksimDudar <maksimdudar1@gmail.com>
2022-08-16 17:16:04 +0300

    add details handler

--- app.rb
+++ app.rb
@@ -44,5 +44,10 @@ post '/new' do
 	@db.execute 'insert into Posts (content, created_date) values (?, datetime())', [content]
 
 redirect to '/'
+end
 
+get '/details/:post_id' do
+	post_id = params[:post_id]
+
+	erb "Displaying infrmation for post with id #{post_id}"
 end
